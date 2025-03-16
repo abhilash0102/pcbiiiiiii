81063484473f689ed27ec6c1257140ae16dd692f
abhilash0102 <165904255+abhilash0102@users.noreply.github.com>
2025-03-16 23:45:38 +0530

    Update api.js

--- pcfront/src/services/api.js
+++ pcfront/src/services/api.js
@@ -1,7 +1,9 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://pcbackbb.onrender.com';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://pcbackbb.onrender.com';
+
+
 
 // Create axios instance
 const api = axios.create({
